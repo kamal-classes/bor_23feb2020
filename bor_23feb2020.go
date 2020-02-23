6ebe1f7d86755afd479e3acf29a908ce9782001e
unknown <kamalsir@ymail.com>
2020-02-23 20:06:08 +0530

    1.0

---
+++ bor_23feb2020.go
@@ -0,0 +1,12 @@
+package bor_23feb2020
+import "fmt"
+
+func Square(num int) {
+res := num * num
+fmt.Println("square = ", res)
+}
+
+func Cube(num int) {
+res := num * num * num
+fmt.Println("cube = ", res)
+}
